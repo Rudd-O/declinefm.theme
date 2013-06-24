9308d3b3a9a529279c4f997c4e00d79354156ca2
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-06-23 23:45:27 -0700

    initial plone commit

---
+++ declinefm/theme/resources/repair.py
@@ -0,0 +1,46 @@
+# following program runs with bin/client1 run ....py
+
+from collections import deque
+from datetime import datetime
+
+import transaction
+from zope.app.component.hooks import setSite
+from Testing.makerequest import makerequest
+from AccessControl.SecurityManagement import newSecurityManager
+
+
+site_id = 'Plone'     # adjust to match your Plone site object id.
+admin_user = 'admin'  # usually 'admin', probably won't need adjusting
+app = makerequest(app)
+site = app["declinefm.com"]["declinefm.com"]
+setSite(site)
+user = app.acl_users.getUser(admin_user).__of__(site.acl_users)
+newSecurityManager(None, user)
+
+
+def treeWalker(root):
+    # stack holds (parent, id, obj) tuples
+    stack = deque([(None, None, root)])
+    while stack:
+        parent, id, next = stack.popleft()
+        try:
+            stack.extend((next, id, child) for id, child in
+next.objectItems())
+        except AttributeError:
+            # No objectItems method
+            pass
+        yield parent, id, next
+
+from collective.flowplayer.interfaces import IFlowPlayerSite
+from five.utilities.marker import erase
+
+count = 0
+for parent, id, obj in treeWalker(site):
+    print "inspecting", id
+    if obj.getLayout() == "flowplayer":
+         obj.setLayout("file_view")
+    if IFlowPlayerSite.providedBy(obj):
+        print 'flowplayersite provided by', id
+        erase(obj, IAudio)
+
+transaction.commit()
