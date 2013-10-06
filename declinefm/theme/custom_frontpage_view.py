22f9f71d34cfb68a67ed9973a05946fedb0753b7
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-10-06 01:36:33 -0700

    use internal access to front page elements to compose the splash text

--- declinefm/theme/custom_frontpage_view.py
+++ declinefm/theme/custom_frontpage_view.py
@@ -1,11 +1,13 @@
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from zope.component.hooks import getSite
+
 class CustomFrontpageView(BrowserView):
 
     template = ViewPageTemplateFile('custom_frontpage_view.pt')
 
     def __call__(self):
         """"""
-        self.blog_posts = getattr(self.context, 'blog')
+        self.splash_text = getSite()['front-page-elements']['splash']
         return self.template()
