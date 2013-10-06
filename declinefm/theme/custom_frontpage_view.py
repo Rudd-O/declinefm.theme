1b9c8a742b6be7906f694e81888c1ac23a6d3519
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-10-06 02:22:26 -0700

    use internal access (viewlets) to compose the rest of the elements of the page

--- declinefm/theme/custom_frontpage_view.py
+++ declinefm/theme/custom_frontpage_view.py
@@ -1,13 +1,10 @@
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-from zope.component.hooks import getSite
-
 class CustomFrontpageView(BrowserView):
 
     template = ViewPageTemplateFile('custom_frontpage_view.pt')
 
     def __call__(self):
         """"""
-        self.splash_text = getSite()['front-page-elements']['splash']
         return self.template()
