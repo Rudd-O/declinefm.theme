a462abd41b25613822698275e0bc8ae6740f03b2
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-10-05 23:32:12 -0700

    remove dumb shit

--- declinefm/theme/custom_frontpage_view.py
+++ declinefm/theme/custom_frontpage_view.py
@@ -8,6 +8,4 @@ class CustomFrontpageView(BrowserView):
     def __call__(self):
         """"""
         self.blog_posts = getattr(self.context, 'blog')
-        self.news_posts = getattr(self.context, 'news')
-        self.archive_posts = getattr(self.context, 'archive')
         return self.template()
